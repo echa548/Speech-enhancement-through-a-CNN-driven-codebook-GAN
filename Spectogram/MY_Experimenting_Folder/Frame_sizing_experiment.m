ee9dc16b7e79e26a49cb0a9dfadb9b92b0d595be
tagu869 <tagu869@aucklanduni.ac.nz>
2023-05-04 21:00:26 +1200

    Updated 16Khz. Data and Audio normalization in progress.
    Updated to 16Khz and 0.032ms window length to capture the periodicity of speech. In addition, audio normalization is in the works and a data pipe-line from external audio sources will be made. Do not commit the samples from the voice bank.

--- Spectogram/MY_Experimenting_Folder/Frame_sizing_experiment.m
+++ Spectogram/MY_Experimenting_Folder/Frame_sizing_experiment.m
@@ -1,10 +1,10 @@
 clear
 clc
 Fs=48000;
-Fs2=11025;
+Fs2=16000;
 Sampling_Frequencies = [Fs,Fs2];
-Frame_dur = 0.04;
+Frame_dur = 0.032;
 Frame_size = Sampling_Frequencies(2)*Frame_dur;
 Lowest_Frequency = 5*Sampling_Frequencies(2)/Frame_size;
 Frequency_resolution = Sampling_Frequencies(2)/Frame_size;
-Temporal_resolution = Frame_size/Sampling_Frequencies(2);
+Temporal_resolution = 1/Frequency_resolution;
--- Spectogram/Spectrogram_generation.py
+++ Spectogram/Spectrogram_generation.py
@@ -4,15 +4,15 @@ import numpy as np
 from scipy import signal
 import scipy.signal as sps
 from scipy.signal import butter, lfilter
-
+from pydub import AudioSegment, effects
 import matplotlib.pyplot as plt
 
-samplerate, data = wavfile.read("MY_Experimenting_Folder/Medium48k.wav")
+samplerate, data = wavfile.read("C:/Users/Admin/Documents/GitHub/COMPSYS-ELECTENG-700/Spectogram/MY_Experimenting_Folder/Medium48k.wav")
 print(type(data), type(samplerate))
 print(data.shape, samplerate)
 
 Fs1 = samplerate
-Fs2 = 11025
+Fs2 = 16000
 N = len(data)
 total_time = (N-1)/Fs1
 Max_Signal_Frequency =Fs2/2
@@ -35,7 +35,7 @@ def butter_lowpass_filter(data, cutoff, fs, order=5):
 for i in range (0,2):  
  Original_signal = data[:,i]
  
- Anti_Aliased_signal = np.array(butter_lowpass_filter(Original_signal,Max_Signal_Frequency-1,Fs1))
+ Anti_Aliased_signal = np.array(butter_lowpass_filter(Original_signal,Max_Signal_Frequency,Fs1))
  Down_sampled_signal = np.array(sps.resample(Anti_Aliased_signal,New_sample_amount))
  
  if i ==0:
@@ -44,48 +44,53 @@ for i in range (0,2):
     Right_channel_Stereo = Down_sampled_signal
 
 print(np.shape(Left_channel_Stereo))
+#Left_channel_Stereo = effects.normalize(Left_channel_Stereo)
+#Right_channel_Stereo = effects.normalize(Right_channel_Stereo)
 Left_channel_right_channel = np.vstack((Left_channel_Stereo, Right_channel_Stereo))
 
 Left_channel_right_channel=Left_channel_right_channel.transpose()
+#normalized_audio = effects.normalize(Left_channel_right_channel)
 #Only uncomment if a file needs to be downsampled
-wavfile.write('MY_Experimenting_Folder/abc1.wav', Fs2, Left_channel_right_channel*169.84)
-
+wavfile.write('C:/Users/Admin/Documents/GitHub/COMPSYS-ELECTENG-700/Spectogram/MY_Experimenting_Folder/abc1.wav', Fs2, Left_channel_right_channel)
+rawsound = AudioSegment.from_file("C:/Users/Admin/Documents/GitHub/COMPSYS-ELECTENG-700/Spectogram/MY_Experimenting_Folder/abc1.wav", "wav")  
+normalizedsound = effects.normalize(rawsound)  
+normalizedsound.export("C:/Users/Admin/Documents/GitHub/COMPSYS-ELECTENG-700/Spectogram/MY_Experimenting_Folder/abc1.wav", format="wav")
 #Change directory to downsampled file of interest
-Down_Sampled_rate, Downsampled_data = wavfile.read('MY_Experimenting_Folder/abc1.wav')
+Down_Sampled_rate, Downsampled_data = wavfile.read('C:/Users/Admin/Documents/GitHub/COMPSYS-ELECTENG-700/Spectogram/MY_Experimenting_Folder/abc1.wav')
 Downsampled_data = Downsampled_data
 Down_Sampled_data_left = Downsampled_data[:,0]
 Down_Sampled_data_right = Downsampled_data[:,1]
 
 #Change directories as required
-f, t, Lxx = signal.spectrogram(x = Down_Sampled_data_left, fs = Down_Sampled_rate, window = 'hann',nperseg = 512,noverlap = 384,nfft = 512,detrend='constant', return_onesided=True, scaling='density', axis=-1, mode='magnitude')
+f, t, Lxx = signal.spectrogram(x = Down_Sampled_data_left, fs = Down_Sampled_rate, window = 'hann',nperseg = 640,noverlap = 480,nfft = 2048,detrend='constant', return_onesided=True, scaling='density', axis=-1, mode='magnitude')
 plt.pcolormesh(t, f, 10 * np.log10(Lxx), cmap ='magma')
 plt.colorbar(label='Decibels')
 plt.ylabel('Frequency [Hz]')
 plt.xlabel('Time [sec]')
-plt.savefig('Spectrogram_Plots\\Magnitude_Plot_Left.png')
+plt.savefig('C:/Users/Admin/Documents/GitHub/COMPSYS-ELECTENG-700/Spectogram/MY_Experimenting_Folder/Spectrogram_Plots/Magnitude_Plot_Left.png')
 plt.close()
 
-f, t, Rxx = signal.spectrogram(x = Down_Sampled_data_right,fs = Down_Sampled_rate,window = 'hann',nperseg = 512,noverlap = 384,nfft =  512,detrend='constant', return_onesided=True, scaling='density', axis=-1, mode='magnitude')
+f, t, Rxx = signal.spectrogram(x = Down_Sampled_data_right,fs = Down_Sampled_rate,window = 'hann',nperseg = 640,noverlap = 480,nfft =  2048,detrend='constant', return_onesided=True, scaling='density', axis=-1, mode='magnitude')
 plt.pcolormesh(t, f, 10 * np.log10(Rxx), cmap ='magma')
 plt.colorbar(label='Decibels')
 plt.ylabel('Frequency [Hz]')
 plt.xlabel('Time [sec]')
-plt.savefig('Spectrogram_Plots\\Magnitude_Plot_Right.png')
+plt.savefig('C:/Users/Admin/Documents/GitHub/COMPSYS-ELECTENG-700/Spectogram/MY_Experimenting_Folder/Spectrogram_Plots/Magnitude_Plot_Right.png')
 plt.close()
-Phase, t_phase, Lxx_Phase = signal.spectrogram( x = Down_Sampled_data_left, fs = Down_Sampled_rate, window = 'hann', nperseg = 512,noverlap = 384,nfft = 512,detrend='constant', return_onesided=True, scaling='density', axis=-1, mode='angle')
+Phase, t_phase, Lxx_Phase = signal.spectrogram( x = Down_Sampled_data_left, fs = Down_Sampled_rate, window = 'hann', nperseg = 640,noverlap = 480,nfft =  2048,detrend='constant', return_onesided=True, scaling='density', axis=-1, mode='angle')
 plt.pcolormesh(t_phase, Phase, Lxx_Phase, cmap ='magma')
 plt.colorbar(label='Phase')
 plt.ylabel('Frequency [Hz]')
 plt.xlabel('Time [sec]')
-plt.savefig('Spectrogram_Plots\\Phase_Plot_Left.png')
+plt.savefig('C:/Users/Admin/Documents/GitHub/COMPSYS-ELECTENG-700/Spectogram/MY_Experimenting_Folder/Spectrogram_Plots/Phase_Plot_Left.png')
 plt.close()
 
-Phase, t_phase, Rxx_Phase = signal.spectrogram(x = Down_Sampled_data_right,fs = Down_Sampled_rate, window = 'hann', nperseg = 512, noverlap = 384, nfft = 512,detrend='constant', return_onesided=True, scaling='density', axis=-1, mode='angle')
+Phase, t_phase, Rxx_Phase = signal.spectrogram(x = Down_Sampled_data_right,fs = Down_Sampled_rate, window = 'hann', nperseg = 640,noverlap = 480,nfft =  2048,detrend='constant', return_onesided=True, scaling='density', axis=-1, mode='angle')
 plt.pcolormesh(t_phase, Phase, Rxx_Phase, cmap ='magma')
 plt.colorbar(label='Phase')
 plt.ylabel('Frequency [Hz]')
 plt.xlabel('Time [sec]')
-plt.savefig('Spectrogram_Plots\\Phase_Plot_Right.png')
+plt.savefig('C:/Users/Admin/Documents/GitHub/COMPSYS-ELECTENG-700/Spectogram/MY_Experimenting_Folder/Spectrogram_Plots/Phase_Plot_Right.png')
 plt.close()
 
 
@@ -120,4 +125,4 @@ print(np.shape(Real_signal_Left))
 #It finally fking works
 Real_signal_Stereo= np.vstack((Real_signal_Left[1,:], Real_signal_Right[1,:]))
 Real_signal_Stereo=Real_signal_Stereo.transpose()
-wavfile.write('MY_Experimenting_Folder/Test_ABC.wav', Fs2, Real_signal_Stereo*5.7)
+wavfile.write('C:/Users/Admin/Documents/GitHub/COMPSYS-ELECTENG-700/Spectogram/MY_Experimenting_Folder/Test_ABC.wav', Fs2, Real_signal_Stereo*5.7)
