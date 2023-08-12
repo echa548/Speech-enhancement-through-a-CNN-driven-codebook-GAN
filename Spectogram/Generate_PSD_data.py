76c98f4ceb9824b7068f7c9a173208ee26168f16
tagu869 <79784395+tagu869@users.noreply.github.com>
2023-08-13 00:33:45 +1200

    Updated data generator

--- Spectogram/Generate_PSD_data.py
+++ Spectogram/Generate_PSD_data.py
@@ -32,54 +32,77 @@ def Hann_window_a_signal(Windowed_data):
  return Windowed_data_fft
 
 
-directories = os.listdir('creating_dataset2/dataset_all4/-6dB')
-directories2 = os.listdir('creating_dataset2/dataset_all4/noise/-6dB')
-Mixture_PSD = np.zeros((len(directories),1024))
-Clean_PSD = np.zeros((len(directories),1024))
+#directories = os.listdir('creating_dataset2/dataset_all4/-6dB')
+#directories2 = os.listdir('creating_dataset2/dataset_all4/noise/-6dB')
+#Mixture_PSD = np.zeros((len(directories),1024))
+#Clean_PSD = np.zeros((len(directories),1024))
 Data_number_Mixture =0
 Data_number_clean = 0
 chunk_length_ms = 32
 samplerate = 16000
-for No_of_data in range (0,len(directories)):
-   myaudio = AudioSegment.from_file("creating_dataset2/dataset_all4/-6dB/"+ directories[No_of_data], "wav") 
+
+dummy_directories = os.listdir('SNR_seg/3dB')
+
+targets = [9,6,3,0,-3,-6,-9]
+Mixture_PSD = np.zeros((len(dummy_directories)*len(targets),1024))
+Clean_PSD = np.zeros((len(dummy_directories)*len(targets),1024))
+
+for target_db in range (0,len(targets)):
+ Path_of_noisy_mixture = 'SNR_seg/'+str(targets[target_db])+'dB'+'/'
+ Path_of_real_noise_files = 'SNR_seg/noise/'+str(targets[target_db])+'dB'+'/'
+ Noise_files = os.listdir('SNR_seg/noise/'+str(targets[target_db])+'dB')
+ Noisy_mixture_files = os.listdir('SNR_seg/'+str(targets[target_db])+'dB')
+
+
+
+ for No_of_data in range (0,len(Noisy_mixture_files)):
+
+   myaudio = AudioSegment.from_file(Path_of_noisy_mixture+ Noisy_mixture_files[No_of_data], "wav") 
    chunks = make_chunks(myaudio, chunk_length_ms)
    chosen_chunk = random.randint(0,len(chunks)-1)
    data = np.array(chunks[chosen_chunk].get_array_of_samples())
    data = np.transpose(data)
    #print(np.shape(data))
-   Bit_Check = wave.open("creating_dataset2/dataset_all4/-6dB/"+ directories[No_of_data], 'rb')
+   Bit_Check = wave.open(Path_of_noisy_mixture+ Noisy_mixture_files[No_of_data], 'rb')
    bit_depth = Bit_Check.getsampwidth() * 8
    data = data/(2**(bit_depth-1))
    Rectangular_windowed_signal = data
    FFT_of_windowed_signal = Hann_window_a_signal(Rectangular_windowed_signal)
    Hann_window = sps.windows.hann(len(Rectangular_windowed_signal))
    PSD_window_scaling = np.sum(Hann_window**2)
-   Mixture_PSD[No_of_data,:] = (np.abs(FFT_of_windowed_signal)**2)/(samplerate*PSD_window_scaling)
+   Mixture_PSD[No_of_data+(len(Noisy_mixture_files)*target_db),:] = (np.abs(FFT_of_windowed_signal)**2)/(samplerate*PSD_window_scaling) 
    myaudio=0
    chunks = 0
-   myaudio = AudioSegment.from_file("creating_dataset2/dataset_all4/noise/-6dB/"+ directories2[No_of_data], "wav") 
+   myaudio = AudioSegment.from_file(Path_of_real_noise_files+ Noise_files[No_of_data], "wav") 
    chunks = make_chunks(myaudio, chunk_length_ms)
    data = np.array(chunks[chosen_chunk].get_array_of_samples())
    #print(data)
    data = np.transpose(data)
    #print(np.shape(data))
-   Bit_Check = wave.open("creating_dataset2/dataset_all4/noise/-6dB/"+ directories2[No_of_data], 'rb')
+   Bit_Check = wave.open(Path_of_real_noise_files+ Noise_files[No_of_data], 'rb')
    bit_depth = Bit_Check.getsampwidth() * 8
    data = data/(2**(bit_depth-1))
    Rectangular_windowed_signal = data
    FFT_of_windowed_signal = Hann_window_a_signal(Rectangular_windowed_signal)
    Hann_window = sps.windows.hann(len(Rectangular_windowed_signal))
    PSD_window_scaling = np.sum(Hann_window**2)
-   Clean_PSD[No_of_data,:] = (np.abs(FFT_of_windowed_signal)**2)/(samplerate*PSD_window_scaling)
+   Clean_PSD[No_of_data+(len(Noisy_mixture_files)*target_db),:] = (np.abs(FFT_of_windowed_signal)**2)/(samplerate*PSD_window_scaling)
    myaudio=0
    chunks = 0
+  
 
 
 
-print(np.max(Mixture_PSD))
-print(np.max(Clean_PSD))
-
-#np.save('Mixture_PSD',Mixture_PSD)
 
 
-#np.save('Clean_PSD',Clean_PSD)
+
+
+
+
+#print(np.max(Mixture_PSD))
+#print(np.max(Clean_PSD))
+
+np.save('Mixture_PSD',Mixture_PSD)
+
+
+np.save('Clean_PSD',Clean_PSD)
